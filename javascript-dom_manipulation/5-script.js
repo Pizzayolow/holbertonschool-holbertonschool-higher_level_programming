9d64bbda3b42de964f5aaf2cee64c1cce88df318
Pizzayolow <ralliard.valentin@gmail.com>
2023-09-11 15:50:09 +0200

    adds all files

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,5 +1,5 @@
 const update = document.getElementById('update_header');
 
 update.addEventListener('click', function () {
-    update.textContent = 'New Header!!!';
+  update.textContent = 'New Header!!!';
 });
--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -2,11 +2,11 @@ const apiUrl = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
 const character = document.getElementById('character');
 
 fetch(apiUrl)
-    .then((response) => {
-        return response.json();
-    })
+  .then((response) => {
+    return response.json();
+  })
 
-    .then((data) => {
-        const characterName = data.name;
-        character.textContent = characterName
-    })
+  .then((data) => {
+    const characterName = data.name;
+    character.textContent = characterName;
+  });
